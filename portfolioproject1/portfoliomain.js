f18646f64e6c30a64653fc5bc6fa44f221c89cce
ritiktig <159533104+ritiktig@users.noreply.github.com>
2025-07-14 01:37:22 +0530

    Add files via upload

---
+++ portfolioproject1/portfoliomain.js
@@ -0,0 +1,8 @@
+var typed = new Typed (".text",
+    {
+    strings:["frontend developer","web developer","ui/ux designer"],
+    typeSpeed:100,
+    backSpeed:100, 
+    backDelay:1000,
+    loop:true 
+        });
